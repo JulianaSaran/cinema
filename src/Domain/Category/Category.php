12170cdd877508ee8446aeae0027a010e4a14fa3
Juliana Saran <julianasarans@gmail.com>
2022-11-22 18:03:02 -0300

    changed created at to private

--- src/Domain/Category/Category.php
+++ src/Domain/Category/Category.php
@@ -7,11 +7,16 @@ use DateTime;
 class Category
 {
     public string $name;
-    public DateTime $createdAt;
+    private DateTime $createdAt;
 
     public function __construct(string $name, DateTime $createdAt)
     {
         $this->name = $name;
         $this->createdAt = $createdAt;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
     }
 }
